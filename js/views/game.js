82758df6c7b9a82f95606cbe0c611f18b96101b4
Michael Murry <mmurry@gmail.com>
2014-05-29 17:54:05 -0500

    Add delete game functionality.
    Signed-off-by: Michael Murry <mmurry@gmail.com>

--- js/views/game.js
+++ js/views/game.js
@@ -5,6 +5,15 @@ app.GameView = Backbone.View.extend({
 	className: 'col-md-4',
 	template: _.template($('#gameTemplate').html()),
 
+	events: {
+		'click .delete': 'deleteGame'
+	},
+
+	deleteGame: function() {
+		this.model.destroy();
+		this.remove();
+	},
+
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
